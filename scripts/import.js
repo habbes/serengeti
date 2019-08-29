6d7c4ee61d02bc0177aa5173a1990b3f6a1f2648
Clément Habinshuti <hello@habbes.xyz>
2019-08-29 23:06:58 +0300

    Parse date during data import

--- scripts/import.js
+++ scripts/import.js
@@ -1,12 +1,22 @@
 const config = require('../config');
 const mongodb = require('mongodb');
 
-const data = require('../data/data.json');
+const rawData = require('../data/data.json');
 
 async function importData () {
-    const client = await mongodb.connect(config.MONGODB_URI);
+    const client = await mongodb.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
     console.log('Importing data...');
-    await client.db().collection(config.COLLECTION).insertMany(data);
+    const db = client.db();
+    // first clear all data
+    await db.createCollection(config.COLLECTION);
+    await db.dropCollection(config.COLLECTION);
+    // pre-process data
+    const data = rawData.map((row) => ({
+        ...row,
+        datetime: new Date(row.datetime)
+    }));
+    // import
+    await db.collection(config.COLLECTION).insertMany(data);
     console.log('Data import complete.');
     process.exit(0);
 }
