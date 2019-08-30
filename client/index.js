6604649d296a0a9aed28956952785ad872258c4b
Clément Habinshuti <hello@habbes.xyz>
2019-08-30 10:31:04 +0300

    Interpolate colors on map

--- client/index.js
+++ client/index.js
@@ -1,58 +1,75 @@
-import { toGeoJSON, runQuery } from './util';
+import { toGeoJSON, runQuery, processData } from './util';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGFiYmVzIiwiYSI6ImNqN3ZsdW5xNjVhNDMzM21sY2Y4Y3d3OHQifQ.5Z4dZGEYlOH2-ToSNskghg';
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/satellite-v9',
-  center: [34.6857, 2.1540],
-  zoom: 9
+  // center: [34.6857, 2.1540],
+  // zoom: 9
 });
 map.addControl(new mapboxgl.NavigationControl());
 
 function init() {
   runQuery([
     { $match: { 'species.name': 'zebra' } },
-    { $limit: 10 }
+    { $limit: 100 }
   ]).then(data => {
-    console.log('ran query');
-    const geoData = toGeoJSON(data);
-    map.on('load', () => {
-      map.addSource('animals', {
-        type: 'geojson',
-        data: {
-          'type': 'FeatureCollection',
-          'features': geoData
-        }
-      });
-      console.log('added source', geoData);
-  
-      map.addLayer({
-        id: 'clusters',
-        type: 'circle',
-        source: 'animals',
-        paint: {
-          'circle-color': {
-              property: 'count',
-              type: 'interval',
-              stops: [
-                  [0, '#41A337'],
-                  [100, '#2D7026'],
-                  [750, '#0B5703'],
-              ]
-          },
-          'circle-radius': {
-              property: 'count',
-              type: 'interval',
-              stops: [
-                  [0, 20],
-                  [100, 30],
-                  [750, 40]
-              ]
-          }
+    console.log('ran query', data);
+    // const geoData = toGeoJSON(data);
+    const bundle = processData(data);
+    // map.setCenter(geoData[0].geometry.coordinates);
+    console.log('new center', bundle);
+
+    map.addSource('animals', {
+      type: 'geojson',
+      data: {
+        'type': 'FeatureCollection',
+        'features': bundle.geoData
       }
-      })
+    });
+    console.log('added source', bundle.geoData);
+    map.fitBounds([
+      [bundle.minLng, bundle.minLat],
+      [bundle.maxLng, bundle.maxLat]
+    ]);
+  
+    map.addLayer({
+      id: 'clusters',
+      type: 'circle',
+      source: 'animals',
+      paint: {
+        // 'circle-color': {
+        //   property: 'count',
+        //     type: 'interval',
+        //     stops: [
+        //       [0, '#41A337'],
+        //       [100, '#2D7026'],
+        //       [750, '#0B5703'],
+        //     ]
+        //   },
+          'circle-color': [
+            'interpolate', ['linear'], ['get', 'timestamp'],
+            bundle.minTimestamp, '#00aa00',
+            bundle.maxTimestamp, '#aaffaa'
+          ],
+          // 'circle-color': '#aaffaa',
+          'circle-opacity': [
+            'interpolate', ['linear'], ['get', 'timestamp'],
+            bundle.minTimestamp, 0.5,
+            bundle.maxTimestamp, 1
+          ],
+        //   'circle-radius': {
+        //     property: 'count',
+        //     type: 'interval',
+        //     stops: [
+        //       [0, 20],
+        //       [100, 30],
+        //       [750, 40]
+        //     ]
+        // }
+      }
     });
   });  
 }
 
-window.onload = init;
+map.on('load', init);
--- client/util.js
+++ client/util.js
@@ -3,7 +3,11 @@ const URL = process.env.NODE_ENV === 'production' ? 'https://serengeti-explorer.
 export function runQuery(query) {
   return fetch(`${URL}/query`, {
     method: 'POST',
-    body: JSON.stringify(query)
+    body: JSON.stringify(query),
+    headers: {
+      'Content-Type': 'application/json',
+      'Accepts': 'application/json'
+    }
   }).then(resp => resp.json());
 }
 
@@ -15,4 +19,56 @@ export function toGeoJSON(data) {
       ...item
     }
   }));
+}
+
+export function processData(data) {
+  let minTimestamp = Infinity;
+  let maxTimestamp = 0;
+  let minLng = Infinity;
+  let maxLng = 0;
+  let minLat = Infinity;
+  let maxLat = 0;
+  const geoData = [];
+
+  data.forEach((item) => {
+    const [lng, lat] = item.geolocation.coordinates;
+    if (lng > maxLng) {
+      maxLng = lng;
+    }
+    if (lng < minLng) {
+      minLng = lng;
+    }
+    if (lat > maxLat) {
+      maxLat = lat;
+    }
+    if (lat < minLat) {
+      minLat = lat;
+    }
+    if (item.timestamp > maxTimestamp) {
+      maxTimestamp = item.timestamp;
+    }
+    if (item.timestamp < minTimestamp) {
+      minTimestamp = item.timestamp;
+    }
+
+    const geoItem = {
+      type: 'feature',
+      geometry: item.geolocation,
+      properties: {
+        ...item
+      }
+    };
+
+    geoData.push(geoItem);
+  });
+
+  return {
+    geoData,
+    minLng,
+    maxLng,
+    minLat,
+    maxLat,
+    minTimestamp,
+    maxTimestamp
+  };
 }
